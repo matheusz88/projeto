6c268a1a360bda81fa46ef7b489f4ae4797824eb
Valeriahild <valeriahildebrandt123@gmail.com>
2025-06-04 15:01:07 -0300

    Add files via upload

--- simulador caixa eletronico.c
+++ simulador caixa eletronico.c
@@ -2,14 +2,19 @@
 #include <stdlib.h>
 
 int main() {
+    int senha = 1234;
+    int senhausuario;
     float saldo = 1000;
     float saque;
     float deposito;
     int escolha;
 
+printf ("digite a senha\n");
+    scanf ("%i", &senhausuario);
 
-
-  while (escolha != 4){
+    if ( senha == senhausuario) {
+        printf ("senha correta\n");
+        while (escolha != 4){
     printf("Caixa eletronico:\n");
 
     printf("Digite a operacao desejada:\n (1)Consulta saldo\n (2)saque\n (3)depósito\n (4)sair:  ");
@@ -47,14 +52,16 @@ int main() {
 case 4:
             printf("Digite sair: \n");
             printf ("Obrigado por utilizar o caixa eletronico");
+
+            default:
+            printf ("numero invalido\n");
   }
 }
+    }
+    else {
+        printf ("senha incorreta\n");
+    }
+
+
 return 0;
 }
-
-
-
-
-
-
-
